83150b5999512710c43c4d0277437eab4c6e7b52
SaiRamyaKamali <srkamali1326@gmail.com>
2020-07-08 17:07:39 +0530

    Network Info

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -7,7 +7,8 @@ import Dishdetail from './DishdetailComponent';
 import Reservation from './ReservationComponent';
 import Favorites from './FavoriteComponent';
 import Login from './LoginComponent';
-import { View, Platform,  Image, StyleSheet, ScrollView, Text} from 'react-native';
+import { View, Platform,  Image, StyleSheet, ScrollView, Text, ToastAndroid} from 'react-native';
+import NetInfo from '@react-native-community/netinfo';
 import {createStackNavigator} from 'react-navigation-stack';
 import {createAppContainer, SafeAreaView} from 'react-navigation';
 import { createDrawerNavigator, DrawerItems } from 'react-navigation-drawer';
@@ -308,8 +309,39 @@ class Main extends Component {
     this.props.fetchComments();
     this.props.fetchPromos();
     this.props.fetchLeaders();
+
+    NetInfo.fetch()
+        .then((connectionInfo) => {
+            ToastAndroid.show('Initial Network Connectivity Type: '
+                + connectionInfo.type + ', effectiveType: ' + connectionInfo.effectiveType,
+                ToastAndroid.LONG)
+        });
+
+      NetInfo.addEventListener(connectionChange => this.handleConnectivityChange(connectionChange))
   }
 
+
+  componentWillUnmount() {
+    NetInfo.removeEventListener(connectionChange => this.handleConnectivityChange(connectionChange))
+  }
+
+  handleConnectivityChange = (connectionInfo) => {
+    switch (connectionInfo.type) {
+        case 'none': 
+            ToastAndroid.show ('You are now offline', ToastAndroid.LONG);
+            break;
+        case 'wifi':
+            ToastAndroid.show ('You are now on WiFi', ToastAndroid.LONG);
+            break;
+        case 'cellular':
+            ToastAndroid.show ('You are now on Cellular', ToastAndroid.LONG);
+            break;
+        case 'unknown' :
+            ToastAndroid.show ('You are now have an Unknown connection', ToastAndroid.LONG);
+            break;
+        default: 
+    }
+  }
   render() {
  
     return (
